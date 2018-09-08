ffc7c8f7f2566e7b6c46b0ed6911c84546664415
NOGA\3kely <mah199645@gmail.com>
2018-09-08 22:58:07 +0200

    new laravel projesdct

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -85,13 +85,13 @@
 <div class="navbar-item has-dropdown is-hoverable">
             <a class="navbar-link">Hey {{Auth::user()->name}}</a>
             <div class="navbar-dropdown is-right" >
-              <a href="/profile" class="navbar-item">
+              <a href="/profile" class="navbar-item" style="text-decoration: none;">
                 <span class="icon">
                   <i class="fa fa-fw fa-user-circle-o m-r-5" style="font-size: 15px"></i>
                 </span>
                <span style="font-size: 20px"> Profile</span>
               </a>
-              <a href="#" class="navbar-item">
+              <a href="#" class="navbar-item" style="text-decoration: none;">
                 <span class="icon">
                   <i class="fa fa-fw fa-cog m-r-5" style="font-size: 15px"></i>
                 </span>
@@ -99,7 +99,7 @@
 </span>
               </a>
               <hr class="navbar-divider">
-              <a href="{{route('logout')}}" class="navbar-item" onclick="event.preventDefault();
+              <a href="{{route('logout')}}" style="text-decoration: none;" class="navbar-item" onclick="event.preventDefault();
                          document.getElementById('logout-form').submit();">
                 <span class="icon">
                   <i class="fa fa-fw fa-sign-out m-r-5" style="font-size: 15px"></i>
