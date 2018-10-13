e30c5cba4b9942a928627c1458aa73a7313528fc
NOGA\3kely <mah199645@gmail.com>
2018-10-14 00:14:52 +0200

    new changes

--- app/Http/Controllers/Auth/FacebookController.php
+++ app/Http/Controllers/Auth/FacebookController.php
@@ -30,9 +30,30 @@ class FacebookController extends Controller
 
     public function handleProviderCallback()
     {
-        $user = Socialite::driver('facebook')->user();
-       // dd($user);
-       return  $user->token;
+        $facebookUser = Socialite::driver('facebook')->user();
+        $existUser = User::where('email',$facebookUser->email)
+           ->first();
+        try
+        {
+            if($existUser){
+                Auth::loginUsingId($existUser->id);
+            }else{
+                $user = new User();
+                $user->name = $facebookUser->name;
+                $user->email = $facebookUser->email;
+                $user->password = md5(rand(1,10000));
+                
+                $user->save();
+                Auth::loginUsingId($facebookUser->id);
+            }
+            return redirect()->to('/');
+
+        }    
+        catch(Exception $e){
+            return 'error';
+        }    
+
+       
     }
 
 }
